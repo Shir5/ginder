31d9dd0551ca5e59ab2f3291b7e533ee0cb7bff5
Shir5 <Vova0120_black@icloud.com>
2024-02-28 20:08:01 +0300

    v 0.1

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
-import Image from "next/image";
+"use client";
+import React from "react";
+import { BackgroundBeams } from "../components/ui/background-beams";
 
 export default function Home() {
   return (
     <main>
-      
-    </main>
+      <div className=" h-screen w-full  bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
+        <div className="max-w-2xl mx-auto p-4">
+          <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-yellow-200 to-yellow-600  text-center font-sans font-bold">
+            GINDER
+          </h1>
+          <p></p>
+          <p className="text-yellow-300 max-w-lg mx-auto my-2 text-4xl text-center relative z-10 br">
+            Find a wingman, teammates and friends.
+          </p>     
+        </div>
+        <BackgroundBeams />
+      </div>
+    </main >
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,10 @@
 import type { Config } from "tailwindcss";
+const defaultTheme = require("tailwindcss/defaultTheme");
+ 
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
 
 const config: Config = {
   content: [
@@ -6,6 +12,8 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
+
   theme: {
     extend: {
       backgroundImage: {
@@ -15,6 +23,17 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [addVariablesForColors],
+  
 };
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
+}
 export default config;
