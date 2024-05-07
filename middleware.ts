929aff7634d27df3c670d72427920646f4d866e1
Shir5 <Vova0120_black@icloud.com>
2024-05-07 21:38:55 +0300

    middleware fixes (private & dynamic routes)

--- middleware.ts
+++ middleware.ts
@@ -1,37 +1,37 @@
-'use server'
-import { NextRequest, NextResponse } from 'next/server';
-import { cookies } from 'next/headers';
-import { decrypt } from './lib/session';
+import { NextRequest, NextResponse } from 'next/server'
+import { cookies } from 'next/headers'
+import { decrypt } from './lib/session'
 
-const publicRoutes = ['/'];
+// 1. Specify protected and public routes
+const publicRoutes = ['/']
 
 export default async function middleware(req: NextRequest) {
-    try {
-        const path = req.nextUrl.pathname;
-        const sessionCookie = cookies().get('session')?.value;
+    const path = req.nextUrl.pathname;
+    const isPublicRoute = publicRoutes.includes(path);
+    const cookie = cookies().get('session')?.value;
 
-        const session = await decrypt(sessionCookie);
+    // Decrypt the session
+    const session = await decrypt(cookie) as { userId: number } | null;
 
-        if (!session) {
-            // If there is no session, redirect to the login page
-            return NextResponse.redirect('/', { status: 307 });
-        }
-
-        // Construct the protected route dynamically based on the user's ID
-        const protectedRoute = `/${session.userId}`;
-
-        if (path !== protectedRoute) {
-            // If it's not a protected route, redirect to the user's route
-            return NextResponse.redirect(protectedRoute, { status: 307 });
-        }
-    } catch (error) {
-        console.error('Error in middleware:', error);
+    // If session is not present, redirect to public route
+    if (!session && !isPublicRoute) {
+        return NextResponse.redirect(new URL('/', req.nextUrl));
     }
 
+    // Construct the protected route dynamically based on the user's ID
+    const protectedRoute = session ? `/${session.userId}` : null;
+
+    // If the requested route doesn't match the protected route, redirect to the user's route
+    if (protectedRoute && path !== protectedRoute) {
+        return NextResponse.redirect(new URL(protectedRoute, req.nextUrl));
+    }
+
+    // If the requested route is a public route or matches the protected route, allow access
     return NextResponse.next();
 }
+
 
 // Routes Middleware should not run on
 export const config = {
     matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
-};
+}
